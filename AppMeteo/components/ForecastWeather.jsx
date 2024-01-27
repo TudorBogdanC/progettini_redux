bed45d61ac7990b59bc6edb8150a66f48f4b38a5
“TudorBogdanC” <toriatteteo@gmail.com>
2024-01-27 12:05:04 +0100

    aggiunto carousel slider per le previsione a breve termine con aggiunta di temp massime e minime

--- AppMeteo/components/ForecastWeather.jsx
+++ AppMeteo/components/ForecastWeather.jsx
@@ -1,36 +1,54 @@
-import { Col } from "react-bootstrap"
+import { Col, Carousel } from "react-bootstrap";
 
-
-
-const ForecastWeather = ({forecast}) => {
-    return (
-        <>
-           <Col className="my-5 mx-3 d-flex flex-column">
-            <small className=" my-4">Previsioni a breve termine</small>
-            <br/>
-              <div className="bottom">
-                <div className="forecast">
-                  <div className="dayOne">
-                    <p className="small">Oggi</p>
-                    <small>
-                      <div><img src={`https://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png`} alt="" height={60} /></div>
-                    </small>
-                  </div>
-                  <div className="dayTwo">
-                    <p className="small">Domani</p>
-                    <small>
-                      <div><img src={`https://openweathermap.org/img/wn/${forecast.list[8].weather[0].icon}@2x.png`} alt="" height={60} /></div>
-                    </small>
-                  </div>
-                  <div className="dayThree">
-                    <p className="small">Dopodomani</p>
-                    <div><img src={`https://openweathermap.org/img/wn/${forecast.list[16].weather[0].icon}@2x.png`} alt="" height={60} /></div>
-                  </div>
-                </div>
-              </div>
-            </Col>
-        </>
-    )
-}
+const ForecastWeather = ({ forecast }) => {
+  return (
+    <Col className="my-5 mx-4">
+      <Carousel>
+        <Carousel.Item>
+          <div className="dayOne">
+            <p className="small">Oggi</p>
+            <small>
+              <img
+                src={`https://openweathermap.org/img/wn/${forecast.list[0].weather[0].icon}@2x.png`}
+                alt=""
+                height={100}
+              />
+            </small>
+            <p>Min / Max</p>
+            <p>{forecast?.list[0].main.temp_min.toFixed()}° C / {forecast?.list[0].main.temp_max.toFixed()}° C </p>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="dayTwo">
+            <p className="small">Domani</p>
+            <small>
+              <img
+                src={`https://openweathermap.org/img/wn/${forecast.list[8].weather[0].icon}@2x.png`}
+                alt=""
+                height={100}
+              />
+            </small>
+            <p>Min / Max</p>
+            <p>{forecast?.list[8].main.temp_min.toFixed()}° C / {forecast?.list[8].main.temp_max.toFixed()}° C </p>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="dayThree">
+            <p className="small">Dopodomani</p>
+            <small>
+            <img
+              src={`https://openweathermap.org/img/wn/${forecast.list[16].weather[0].icon}@2x.png`}
+              alt=""
+              height={100}
+            />
+            </small>
+            <p>Min / Max</p>
+            <p>{forecast?.list[16].main.temp_min.toFixed()}° C / {forecast?.list[16].main.temp_max.toFixed()}° C </p>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </Col>
+  );
+};
 
 export default ForecastWeather;
